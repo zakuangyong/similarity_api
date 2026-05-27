8c5a74a524a14c98a4ef4f044c2a99d7d0cab86b
zakuangyong <kuangyong@carzsoft.com>
2026-05-27 18:16:04 +0800

    ui change

---
+++ ui/__init__.py
@@ -0,0 +1,1 @@
+"""Streamlit UI package for the similarity workbench."""
---
+++ ui/detail.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from .components import _render_detail_view, _render_top5_pentagon
+
+__all__ = ["_render_detail_view", "_render_top5_pentagon"]
